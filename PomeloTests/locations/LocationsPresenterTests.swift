79717eb31e48eda8a93aaae83c2edf7fff46a97b
Viliam Straka <straka.viliam@gmail.com>
2020-07-28 19:55:17 +0200

    add presenter tests add view controller tests

--- PomeloTests/locations/LocationsPresenterTests.swift
+++ PomeloTests/locations/LocationsPresenterTests.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2020 Viliam Straka. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import SwiftyJSON
+@testable import Pomelo
+
+class LocationsPresenterTests: XCTestCase {
+    var sut: LocationsPresenter!
+    var spyDisplay: LocationsDisplaySpy?
+    
+    override func setUp() {
+      super.setUp()
+      spyDisplay = LocationsDisplaySpy()
+      sut = LocationsPresenter(view: spyDisplay!)
+    }
+    
+    func testShowError() {
+        //MARK: - When
+        sut.showError(error: ResultError.defaultError())
+        //MARK: - Then
+        XCTAssertTrue(spyDisplay!.showErrorCalled)
+        XCTAssertFalse(spyDisplay!.showLocationsCalled)
+    }
+    
+    func testShowResult() {
+        //MARK: - When
+        sut.showLocations(locations: [])
+        //MARK: - Then
+        XCTAssertTrue(spyDisplay!.showLocationsCalled)
+        XCTAssertFalse(spyDisplay!.showErrorCalled)
+    }
+}
--- PomeloTests/locations/LocationsViewControllerTests.swift
+++ PomeloTests/locations/LocationsViewControllerTests.swift
@@ -6,4 +6,95 @@
 //  Copyright © 2020 Viliam Straka. All rights reserved.
 //
 
-import Foundation
+import XCTest
+import SwiftyJSON
+@testable import Pomelo
+
+class LocationsViewControllerTests: XCTestCase {
+    var window: UIWindow!
+    var sut: LocationsViewController!
+    var spyInteractor: LocationsInteractorSpy?
+    
+    override func setUp() {
+        super.setUp()
+        window = UIWindow()
+        spyInteractor = LocationsInteractorSpy()
+        sut = LocationsViewController()
+        sut.locationsInteractor = spyInteractor
+    }
+    
+    override func tearDown() {
+        window = nil
+        super.tearDown()
+    }
+    
+    func loadView() {
+      window.addSubview(sut.view)
+      RunLoop.current.run(until: Date())
+    }
+    
+    func testCallGetLocations() {
+        //MARK: - Given
+        
+        //MARK: - When
+        loadView()
+        sut.getLocations()
+        
+        //MARK: - Then
+        XCTAssertTrue(spyInteractor!.loadLocationsCalled)
+    }
+    
+    func testDisplayEmptyData() {
+        //MARK: - Given
+        let locations: [PickupLocationModel] = []
+        //MARK: - When
+        loadView()
+        sut.showLocations(locations: locations)
+        
+        //MARK: - Then
+        XCTAssertFalse(sut.noDataLbl.isHidden)
+    }
+    
+    func testDisplayLocationWithoutDistance() {
+        //MARK: - Given
+        let pickupLocationModel = PickupLocationModel.init(fromJSON: JSON([
+            "latitude": 1.0,
+            "longitude": 1.0,
+            "active": true
+        ]))
+        let locations: [PickupLocationModel] = [pickupLocationModel]
+        //MARK: - When
+        loadView()
+        sut.showLocations(locations: locations)
+        
+        //MARK: - Then
+        XCTAssertTrue(sut.noDataLbl.isHidden, "no data label is hidden")
+        XCTAssertEqual(sut.tableView(sut.tableView, numberOfRowsInSection: 0), 1, "in table is 1 row")
+        
+        let cell = sut!.tableView(sut.tableView, cellForRowAt: IndexPath.init(row: 0, section: 0)) as? LocationTableViewCell
+        XCTAssertNotNil(cell, "cell is LocationTableViewCell type")
+        XCTAssertEqual(cell?.distanceLbl.text, "", "distance in cell is empty")
+    }
+    
+    func testDisplayLocationWithDistance() {
+        //MARK: - Given
+        let pickupLocationModel = PickupLocationModel.init(fromJSON: JSON([
+            "latitude": 1.0,
+            "longitude": 1.0,
+            "active": true
+        ]))
+        pickupLocationModel.distance = 111
+        let locations: [PickupLocationModel] = [pickupLocationModel]
+        //MARK: - When
+        loadView()
+        sut.showLocations(locations: locations)
+        
+        //MARK: - Then
+        XCTAssertTrue(sut.noDataLbl.isHidden, "no data label is hidden")
+        XCTAssertEqual(sut.tableView(sut.tableView, numberOfRowsInSection: 0), 1, "in table is 1 row")
+        
+        let cell = sut!.tableView(sut.tableView, cellForRowAt: IndexPath.init(row: 0, section: 0)) as? LocationTableViewCell
+        XCTAssertNotNil(cell, "cell is LocationTableViewCell type")
+        XCTAssertEqual(cell?.distanceLbl.text, "111.0 km", "distance in cell is not empty")
+    }
+}
